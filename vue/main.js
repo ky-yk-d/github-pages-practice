54ca01dad2840ffadabdbd46047cfea5eade976e
ky-yk-d <key0of7one0s5life@gmail.com>
2018-05-04 08:33:06 +0900

    vueを追加

---
+++ vue/main.js
@@ -0,0 +1,27 @@
+var vm = new Vue({
+    el: '#tasks-index',
+    data: {
+        tasks:[
+            {id: 1, name: 'Studying JavaScript', isDeleted: false},
+            {id: 2, name: 'Studying PHP', isDeleted: false},
+            {id: 3, name: 'Studying Ruby', isDeleted: false}
+        ],
+        newTask: '',
+    },
+    methods: {
+        createTask: function(){
+            var new_id = this.tasks[this.tasks.length - 1].id + 1;
+            this.tasks.push({id: new_id, name: this.newTask, idDeleted: false});
+            this.newTask = '';
+        },
+        doneTask: function(task_id){
+            this.tasks.forEach(function(task){
+                if (task.id === task_id){
+                    return task.isDeleted = true;
+                }
+            })
+        }
+    }
+})
+
+window.vm = vm;
